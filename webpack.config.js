1de309be1f8ac7424641789378ac1e40199595ad
Michael Bowman <bowmanmc@gmail.com>
2017-07-23 20:40:28 -0400

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public', 'assets', 'scripts')
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader']
+            }, {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devServer: {
+        publicPath: '/assets/scripts/',
+        contentBase: path.join(__dirname, 'public'),
+        port: 9000
+    }
+};
